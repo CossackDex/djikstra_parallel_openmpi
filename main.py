9d53fb980397d705033e331a693c321ffdb8c1af
dex <d.matuszczyk98@gmail.com>
2022-05-10 21:43:25 +0200

    main and initial testing graph

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import scipy
-
+import numpy as np
+from mpi4py import MPI
 
 
 if __name__ == '__main__':
-    print("Hello World!")
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if rank == 0:
+        # A B C D E
+        graph = np.array([[np.inf, 2, np.inf, 4, np.inf],
+                          [np.inf, 3, 3, np.inf, np.inf],
+                          [np.inf, np.inf, np.inf, np.inf, 2],
+                          [np.inf, 2, np.inf, 4, np.inf],
+                          [np.inf, 2, np.inf, 4, np.inf]])
